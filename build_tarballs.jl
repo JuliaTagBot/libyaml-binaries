f427e64233098fb9e95337a744f674fdf8adafc2
Erik Schnetter <schnetter@gmail.com>
2019-03-11 10:00:11 -0400

    Build MacOS tarballs on MacOS

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -9,7 +9,6 @@ version = v"0.2.1"
 sources = [
     "https://github.com/yaml/libyaml/archive/0.2.1.tar.gz" =>
     "1d2aeb87f7d317f1496e4c39410d913840714874a354970300f375eec9303dc4",
-
 ]
 
 # Bash recipe for building across all platforms
@@ -25,19 +24,30 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
+platforms_base = [
+    FreeBSD(:x86_64),
     Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
     Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
     Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    # MacOS(:x86_64),
-    FreeBSD(:x86_64)
+    Linux(:i686, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:x86_64, libc=:musl),
 ]
+platforms_osx = [
+    MacOS(:x86_64),
+]
+
+os = get(ENV, "TRAVIS_OS_NAME", nothing)
+if os == "linux"
+    platforms = platforms_base
+elseif os == "osx"
+    platforms = platforms_osx
+else
+    platforms = [platforms_base; platforms_osx]
+end
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -46,9 +56,8 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
+build_tarballs(
+    ARGS, name, version, sources, script, platforms, products, dependencies)
