e2fc1ae0c22f597bb0d95c19fea5cb120069aa23
Erik Schnetter <schnetter@gmail.com>
2019-03-10 23:19:48 -0400

    Add autogenerated files to build libyaml

---
+++ README.md
@@ -0,0 +1,9 @@
+# libyaml-binaries
+
+[![Build Status](https://travis-ci.org/eschnett/libyaml-binaries.svg?branch=master)](https://travis-ci.org/eschnett/libyaml-binaries)
+
+This repository builds binary artifacts for the libyaml project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/eschnett/libyaml-binaries/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,54 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libyaml"
+version = v"0.2.1"
+
+# Collection of sources required to build libyaml
+sources = [
+    "https://github.com/yaml/libyaml/archive/0.2.1.tar.gz" =>
+    "1d2aeb87f7d317f1496e4c39410d913840714874a354970300f375eec9303dc4",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd libyaml-0.2.1
+./bootstrap
+./configure --prefix=$prefix --host=$target
+make
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libyaml", :libyaml)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
